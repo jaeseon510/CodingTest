4fb1fd22efa42b545c71883bff6a0f6f6266d5d5
Seon <jaeseon510@naver.com>
2024-12-29 20:57:09 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cout << 2000 << '\n';
+	for (int i = 0; i < 1000; i++) cout << 1 << ' ';
+	for (int i = 0; i < 1000; i++) cout << 1000 << ' ';
+}
