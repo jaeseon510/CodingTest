33254afefad00eef803b574ba183ee5c73a2d6b2
Seon <jaeseon510@naver.com>
2024-12-30 19:10:27 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int N,key;
+	scanf("%d",&N);
+	int arr[2001]={0,};
+	for(int i=0;i<N;i++){
+		scanf("%d",&key);
+		arr[key+1000]=1;
+	}
+	for(int i=0;i<2001;i++){
+		if(arr[i]==1)
+		{
+			printf("%d ",i-1000);
+		}
+	}
+	return 0;
+}
