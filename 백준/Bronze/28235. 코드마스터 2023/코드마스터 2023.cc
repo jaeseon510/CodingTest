16dd308a488d37f09f0b2d55a847fb739c6488c3
Jaeseon <jaeseon510@naver.com>
2025-03-18 04:44:57 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	char s;
+	cin >> s;
+	if (s == 'S') cout << "HIGHSCHOOL";
+	if (s == 'C') cout << "MASTER";
+	if (s == '2') cout << "0611";
+	if (s == 'A') cout << "CONTEST";
+}
