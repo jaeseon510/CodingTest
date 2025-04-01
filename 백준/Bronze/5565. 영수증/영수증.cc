796830b37672a817030fb5105eef69fd185abcea
Jaeseon <jaeseon510@naver.com>
2025-04-01 22:01:27 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int totalPrice;
+	int sum = 0;
+	int num;
+
+	cin >> totalPrice;
+
+	for (int i = 0; i < 9; ++i) {
+		cin >> num;
+		sum += num;
+	}
+
+	cout << totalPrice - sum << endl;
+
+	return 0;
+}
