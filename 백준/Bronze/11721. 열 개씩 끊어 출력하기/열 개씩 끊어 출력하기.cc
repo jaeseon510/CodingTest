3e86705b0b88af9f1b5d15267cf4d75c314e9a44
Jaeseon <jaeseon510@naver.com>
2025-06-22 23:37:59 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <string.h>
+
+int main()
+{
+	char str[101];
+	scanf("%s", str);
+
+	for (int i = 0; i < strlen(str); i++) {
+		printf("%c", str[i]);
+		if ((i+1) % 10 == 0) {
+			printf("\n");
+		}
+	}
+
+	return 0;
+}
