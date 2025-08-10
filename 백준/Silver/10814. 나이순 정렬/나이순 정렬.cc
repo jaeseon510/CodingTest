11fe56b5909944e940499e1295cea436edf9e933
Jaeseon <jaeseon510@naver.com>
2025-08-10 22:59:27 +0900

    [Silver V] Title: 나이순 정렬, Time: 64 ms, Memory: 7904 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+bool compare(pair<int, string>a, pair<int, string>y) {
+	return a.first < y.first;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	vector<pair<int,string>>arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i].first >> arr[i].second;
+	}
+	stable_sort(arr.begin(), arr.end(),compare);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i].first << " " << arr[i].second << "\n";
+	}
+}
