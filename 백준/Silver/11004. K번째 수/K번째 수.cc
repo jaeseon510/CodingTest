c1f818e9ee5fbee2904b8e327d23dcbd4c1807ba
Jaeseon <jaeseon510@naver.com>
2025-09-08 22:38:27 +0900

    [Silver V] Title: K번째 수, Time: 1060 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+	cout << arr[k - 1];
+}
