411e0c41045eef5ffab5c3106f1847db4afae8eb
Jaeseon <jaeseon510@naver.com>
2025-09-09 02:22:01 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<queue>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<utility>
+using namespace std;
+int n;
+priority_queue<int, vector<int>> p_queue;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		if (temp == 0) {
+			if (p_queue.empty()) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << p_queue.top() << '\n';
+				p_queue.pop();
+			}
+		}
+		else {
+			p_queue.push(temp);
+		}
+	}
+}
