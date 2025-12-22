cc4b3373316a7a84a05c1a4de249385154fe2b53
Jaeseon <jaeseon510@naver.com>
2025-12-22 23:31:17 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<string>
+using namespace std;
+#define X first
+#define Y second
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	string a, b, c;
+	cin >> a >> b >> c;
+    if (c[0] >= 48 && c[0] <= 57) {
+        int x = stoi(c) + 1;
+        if (x % 3 == 0) {
+            if (x % 5 == 0) {
+                cout << "FizzBuzz";
+            }
+            else {
+                cout << "Fizz";
+            }
+        }
+        else if (x % 5 == 0) {
+            cout << "Buzz";
+        }
+        else {
+            cout << x;
+        }
+    }
+    else if (b[0] >= 48 && b[0] <= 57) {
+        int x = stoi(b) + 2;
+        if (x % 3 == 0) {
+            if (x % 5 == 0) {
+                cout << "FizzBuzz";
+            }
+            else {
+                cout << "Fizz";
+            }
+        }
+        else if (x % 5 == 0) {
+            cout << "Buzz";
+        }
+        else {
+            cout << x;
+        }
+    }
+    else if (a[0] >= 48 && a[0] <= 57) {
+        int x = stoi(a) + 3;
+        if (x % 3 == 0) {
+            if (x % 5 == 0) {
+                cout << "FizzBuzz";
+            }
+            else {
+                cout << "Fizz";
+            }
+        }
+        else if (x % 5 == 0) {
+            cout << "Buzz";
+        }
+        else {
+            cout << x;
+        }
+    }
+    return 0;
+}
